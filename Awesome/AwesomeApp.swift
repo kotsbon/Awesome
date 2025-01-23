34095cf64a28ff1481aa1e0deeb1f9a012e9426a
JW <worladge@icloud.com>
2025-01-23 19:08:15 +1100

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by James Worladge on 23/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Awesome
+//
+//  Created by James Worladge on 23/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
