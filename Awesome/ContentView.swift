dc9751a58db8c8480bea6d3b91e0c0b8074a46db
JW <worladge@icloud.com>
2025-01-26 00:35:08 +1100

    Ch. 1.3 Button styles, spacers, Frames ...

--- Awesome/ContentView.swift
+++ Awesome/ContentView.swift
@@ -13,20 +13,34 @@ struct ContentView: View {
         
         VStack {
             
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy )
-                .foregroundStyle(.red)
+                .fontWeight(.ultraLight)
                 .padding()
-
-            Button("Click Me!") {
-                message = "Awesome"
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great") {
+                    message = "Great!"
+                }
+                
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
         
