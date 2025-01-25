fc09a5f4b18bc29070e6b36fd8ea9c437a16439f
JW <worladge@icloud.com>
2025-01-25 15:23:06 +1100

    Updated text: capitalised App.

--- Awesome/ContentView.swift
+++ Awesome/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am an app developer!")
+            Text("I am an App developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy )
                 .foregroundStyle(.red)
