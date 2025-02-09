bdadcd6d0dfdec23261f9bc97e7947df3b7fb43d
JW <worladge@icloud.com>
2025-02-09 22:18:15 +1100

    Added ramonizer to button!

--- Awesome/ContentView.swift
+++ Awesome/ContentView.swift
@@ -10,15 +10,15 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
-    
-    var messages = ["Go Stephy!", "You're Hair is Amazing!", "You Will Kick Arsenal's Ass!", "You're a Legend!"]
     @State private var messagesCount = 0
+    
+    var messages = ["Go Stephy!", "You're Hair is Amazing!", "You Will Kick Arsenal's Ass!", "You're a Legend!", "Today is Your Day!", "Hooray!", "You're Amazing!", "You're a Star!"]
+    
     
     var body: some View {
         
         VStack {
             
-        
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -35,24 +35,29 @@ struct ContentView: View {
                 .shadow(radius: 30)
                 .animation(.default, value: imageName)
             
-
+            
             
             Spacer()
             
             Button("Show Message") {
-                imageName = "image\(imageNumber)"
-                imageNumber += 1
                 
-                message = messages[messagesCount]
-                messagesCount += 1
+                imageName = "image\(Int.random(in: 0...9))"
                 
-                if messagesCount >= messages.count {
-                    messagesCount = 0
-                }
+                message = messages[Int.random(in: 0..<messages.count)]
                 
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
+                //                imageName = "image\(imageNumber)"
+                //                imageNumber += 1
+                //
+                //                message = messages[messagesCount]
+                //                messagesCount += 1
+                
+                //                if messagesCount >= messages.count {
+                //                    messagesCount = 0
+                //                }
+                //
+                //                if imageNumber > 9 {
+                //                    imageNumber = 0
+                //                }
                 
             }
             .buttonStyle(.borderedProminent)
